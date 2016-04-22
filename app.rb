ba8a1c5c56b3b7363c03bbded8a78a998032efb3
xiaoey <xiaoeyzhu@gmail.com>
2016-04-22 02:59:56 +0000

    Done.

--- app.rb
+++ app.rb
@@ -11,6 +11,7 @@ class App < Sinatra::Base
   end
 
   post '/display_puppy' do
+#   @puppy = Puppy.new(params[:name], params[:breed], params[:age])
     @name = params[:name]
     @breed = params[:breed]
     @age = params[:age]
