38bdb18d2dda6285dbd0d9893fba86f0f15e66ca
Motti Mordechay Gottlieb <mottigottlieb@gmail.com>
2016-06-10 01:08:02 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,23 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/new' do
+    @name= params[:name]
+    @breed = params[:breed]
+    @age = params[:age]
+    @new_instance = Puppy.new(@name,@breed,@age)
+    @n_name = @new_instance.name
+    @n_breed = @new_instance.breed
+    @n_age = @new_instance.age
+
+      erb :display_puppy
+  end
 end
