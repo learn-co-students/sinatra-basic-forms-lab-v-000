fb6a47dc50c432a26ab9bd434512edf22c38c39b
Han Lee <hanlee12@gmail.com>
2016-07-15 19:48:15 +0000

    initial commit

--- app.rb
+++ app.rb
@@ -1,5 +1,15 @@
 require_relative 'config/environment'
+require_relative "models/puppy.rb"
 
 class App < Sinatra::Base
+
+  get '/' do
+#    erb :index
+  end
+
+  post '/create_puppy' do
+#    @phrase = params[:user_phrase]
+    erb :display_puppy
+  end
 
 end
