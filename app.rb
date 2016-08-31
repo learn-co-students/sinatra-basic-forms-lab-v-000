bc04a0fa50433e917b5f24dad5d981304cfc99b0
Ben Norris <bennorrismisc@gmail.com>
2016-08-31 20:07:44 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,19 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+get '/' do
+  erb :index
+end
+
+get '/new' do
+  erb :create_puppy
+end
+
+post '/new' do
+  @new_name = params[:name]
+  @new_breed = params[:breed]
+  @new_age = params[:age]
+  erb :display_puppy
+end
+
 end
