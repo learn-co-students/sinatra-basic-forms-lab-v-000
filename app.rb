c6db94f52d96077934bef1722bafad1534bc501f
William Jones <enquiries@forestfed.com>
2016-09-01 14:28:46 +0000

    more mvc proctice

--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get "/" do
+  	erb :index
+  end
+  get "/new" do
+  	erb :create_puppy
+  end
+  post '/display' do
+    pup = Puppy.new(params[:name], params[:breed], params[:age],)
+    @name = pup.name
+    @breed = pup.breed
+    @age = pup.age
+    erb :display_puppy
+  end
 
 end
