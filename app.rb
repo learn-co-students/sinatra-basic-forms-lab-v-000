c1b34458496a7fc324ceb1a2812eb2082579f26b
hilaryml <hilyttle@gmail.com>
2016-09-19 13:50:03 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
 require_relative 'config/environment'
+require_relative 'models/puppy.rb'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
+  post '/' do
+    erb :display_puppy
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
 
 end
