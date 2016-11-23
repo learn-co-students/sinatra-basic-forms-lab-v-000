46cc9e02f399cf17aa20c7a07d03cab85e3a946c
Ray Mulligan <rmullig2@optonline.net>
2016-11-23 22:07:52 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,27 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/new' do
+  #  binding.pry
+    name = params[:name]
+    breed = params[:breed]
+    age = params[:age]
+    @puppy = Puppy.new(name, breed, age)
+    erb :display_puppy
+  end
+
+  post '/' do
+    erb :display_puppy
+  end
 
 end
