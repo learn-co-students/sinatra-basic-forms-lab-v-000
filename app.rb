0d07fee662efdd26c094359eb987023466445c63
Camille Jackson <cj@mail.co.de>
2017-05-05 02:15:24 +0000

    Done.

--- app.rb
+++ app.rb
@@ -6,4 +6,12 @@ class App < Sinatra::Base
     erb :index
   end
 
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/' do
+    @puppy = Puppy.new(params[:name],params[:breed],params[:age])
+    erb :display_puppy
+  end
 end
