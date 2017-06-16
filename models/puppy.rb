db0036854addde21a565b3078663c58b02db6cca
Will-Sauer <will@heueso.com>
2017-06-16 12:04:29 -0700

    class built

---
+++ models/puppy.rb
@@ -0,0 +1,23 @@
+class Puppy
+
+#------------------------------------------------------------------
+#macros etc
+attr_accessor :name, :age, :breed
+
+def initialize (name, breed, age)
+    @name = name
+    @age = age
+    @breed = breed    
+end
+
+#------------------------------------------------------------------
+#instance
+
+#------------------------------------------------------------------
+#class
+
+
+
+#oc
+end
+
