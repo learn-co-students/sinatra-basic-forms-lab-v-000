8cd2f9288f2ff622e663e5b7289d1150516ff777
Dustin Anderson <dandersonsoftware@outlook.com>
2017-07-11 12:39:37 +0000

    Done.

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
-  post '/' do
+  post '/puppy' do
     @pups = Puppy.new(params[:name], params[:breed], params[:age])
     erb :display_puppy
   end
