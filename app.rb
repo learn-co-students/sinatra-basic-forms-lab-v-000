4db41db4e0431090fda063f75d62d2dd8dc20efb
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2017-07-25 14:35:35 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,20 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+  get '/create_puppy' do
+    erb :create_puppy
+  end
+
+  post '/display_puppy' do
+    erb :display_puppy
+  end
+
 end
