305320c81975de3887db436eb3872f0e9206497e
Nicholas D'Amico <nickalan82@icloud.com>
2017-08-21 22:44:49 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,21 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  #In app.rb build out a GET request to load a home page. The home page should go to the main route /
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/new' do
+    @name = params[:name]
+    @breed = params[:breed]
+    @age = params[:age]
+
+    erb :display_puppy
+  end
+
 end
