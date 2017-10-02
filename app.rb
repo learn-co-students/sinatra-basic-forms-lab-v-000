eba2fb2cfbd8b62232f3ccd046b370c3335c05e8
Jacqlene Sifre <jsifre01@gmail.com>
2017-10-02 01:16:29 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,24 @@
 require_relative 'config/environment'
+require_relative 'models/puppy.rb'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
+  post '/' do
+    erb :index
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/new' do
+    @dog = params
+
+    erb :display_puppy
+  end
 
 end
