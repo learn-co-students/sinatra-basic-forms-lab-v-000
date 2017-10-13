e710f2da9fc3d01308ea80709fa79ce5e7831c7c
veloblank <veloblank@gmail.com>
2017-10-13 14:07:20 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,20 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    #status 200
+    erb :index
+  end
+
+  get '/new' do
+    #status 200
+    erb :create_puppy
+  end
+
+  post '/puppy' do
+    
+    @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+    erb :display_puppy
+  end
+
 end
