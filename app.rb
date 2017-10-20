1a6547d39d5fddc3f4aaaccd551b653c07f11c92
Janusz Szubert <theinvalidnonce@gmail.com>
2017-10-20 20:02:57 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,24 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  # Render the index page with the link to the puppy form
+  get '/' do
+    erb :index
+  end
+
+  # Get request to /new which has the create_puppy.erb file & form
+  get '/new' do
+    erb :create_puppy
+  end
+
+  # Take user input to POST to display_puppy.erb file/page
+  post '/' do
+    # Assign input parameters to instance variables
+    @name = params[:name]
+    @breed = params[:breed]
+    @age = params[:age]
+
+    erb :display_puppy
+  end
+
 end
---
+++ models/puppy.rb
@@ -0,0 +1,10 @@
+class Puppy
+  attr_accessor :name, :breed, :age
+
+  # Initialize puppy with attributes, the order must be like this
+  def initialize(name, breed, age)
+    @name, @breed, @age = name, breed, age
+  end
+
+
+end
