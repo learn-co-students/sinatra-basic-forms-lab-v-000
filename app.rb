4ed204ceb1c7d4dc35d1b3ea26dbdabea17347a4
chrisjamesr <chrisjames.r@gmail.com>
2017-10-22 20:31:16 -0400

    Done.

--- app.rb
+++ app.rb
@@ -6,17 +6,14 @@ class App < Sinatra::Base
     erb :index
   end
 
-  post '/' do
-    binding.pry
-    
-    erb :display_puppy
-  end
-
   get '/new' do
-
+    
     erb :create_puppy
   end
 
-
+  post '/' do  
+    @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+    erb :display_puppy
+  end
 
 end  #  End of Class
