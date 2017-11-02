3b7ea27f69adc09cd4add4d5ad6ab35eec3da951
Jacilyn Hayden <jacihayden@gmail.com>
2017-11-02 06:10:32 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,24 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :index
+  end
 
+  get '/new' do
+    erb :welcome
+  end
+
+  post '/' do
+    @phrase = PigLatinizer.new
+    @initial_sentence = params[:user_phrase]
+
+    erb :piglatin_results
+  end
 end
+# GET /
+#    renders welcome
+#  GET /NEW
+#    renders the form
+#  POST /
+#    displays the puppy
---
+++ models/puppy.rb
@@ -0,0 +1,16 @@
+class Puppy
+  attr_accessor :name, :breed, :age
+
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+  end
+end
+# Puppy class can create a new instance of the puppy class
+# Puppy class can read a puppy name
+# Puppy class can read a puppy breed
+# Puppy class can read a puppy age
+# Puppy class can change puppy age
+# Puppy class can change puppy name
+# Puppy class can change puppy breed
