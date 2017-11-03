39032f7ac5d034d8338a486d5c1790bc9740b703
Denis Bondin <dsbondin@gmail.com>
2017-11-03 22:33:48 +0000

    Done.

--- app.rb
+++ app.rb
@@ -10,4 +10,9 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
+  post '/puppy' do
+    @puppy = Puppy.new(params[:name], params[:breed], params[:age], )
+    erb :display_puppy
+  end
+
 end
