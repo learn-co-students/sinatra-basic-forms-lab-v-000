fcbd3ac9bc1c79315366b3a6e0e7874af49433da
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-16 00:40:41 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+   get '/' do
+    erb :index
+   end
 
+   post '/create_puppy' do
+     erb :create_puppy
+   end
+
+  get '/create_puppy' do
+    erb "display_puppy"
+  end
 end
