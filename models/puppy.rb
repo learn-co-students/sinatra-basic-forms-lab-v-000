6e93c037930346c24c7ef57fce7e843aa5895e79
Hannah Carnes <32318292+hcarnes@users.noreply.github.com>
2018-01-09 19:08:33 -0500

    use correct form of Struct.new

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,3 +1,3 @@
 #defines initalize method that takes parameters in order creates attr_accessor methods (strut stands for structure)
-class Puppy < Struct.new(:name, :breed, :age)
+Puppy = Struct.new(:name, :breed, :age) do
 end
