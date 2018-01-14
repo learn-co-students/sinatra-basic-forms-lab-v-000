a5d319a3c15a8cc59ac43c9398819999d4b76774
Kaitlin Kelly <kk504408@gmail.com>
2018-01-14 22:27:59 +0000

    Done.

--- app.rb
+++ app.rb
@@ -11,9 +11,8 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
-  post '/' do
+  post '/puppy' do
     @new_puppy = Puppy.new(params[:name], params[:breed], params[:age])
-
     erb :display_puppy
   end
 
