be5d1e34e161e88218f29e2208e8ada6ae9df54a
PJ Frias <peejfrias@gmail.com>
2018-02-02 16:38:41 +0000

    add create_puppy form

--- app.rb
+++ app.rb
@@ -1,5 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    erb :index
+  end
+  
+  get '/new' do
+    erb :create_puppy
+  end
+  
+  post '/new' do
+    @puppy = Puppy.new
+    @puppy.name = params[:name]
+    @puppy.breed = params[:breed]
+    @puppy.age = params[:age]
+    erb :display_puppy
+  end
 
 end
