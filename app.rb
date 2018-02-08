93a78c513c4fa2d42d58b575d473b8e99d0011e3
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-02-08 18:27:28 -0500

    Rspeclemented post '/'. ALL TESTS PASSING

--- app.rb
+++ app.rb
@@ -8,4 +8,14 @@ class App < Sinatra::Base
     get '/new' do
         erb :create_puppy
     end
+
+    post '/new' do
+        @puppy = Puppy.new(
+            params[:name],
+            params[:breed],
+            params[:age]
+        )
+
+        erb :display_puppy
+    end
 end
