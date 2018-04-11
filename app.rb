3d91e61cac1db6c36f6c7df76c38ff3640b289cd
Sam. O <samrey2018@gmail.com>
2018-04-11 05:03:23 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,22 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/'do
+    erb :index  #the home page
+  end
+
+  post '/new' do
+
+    erb :create_puppy
+  end
+
+  post '/'do
+    @name = params["n"]
+    @breed = params["b"]
+    @age = params["a"]
+
+    erb :third_view
+  end
+
+
 end
