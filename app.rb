b074f4c9aec2238073cfa43cd15f4d23b5566dea
Michael St. Marie <michael.landrum.st.marie@gmail.com>
2018-04-17 02:52:04 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,26 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do
+
+    erb :create_puppy
+
+  end
+
+  post '/' do
+    puts params
+
+    Puppy.new(params[:name],params[:breed],params[:age])
+    @name = params[:name]
+    @breed = params[:breed]
+    @age = params[:age]
+
+    erb :display_puppy
+  end
+
+
 end
