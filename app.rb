de75625cbf1f8ff268b4e420520700c8027f7bd3
Annie Bourke <aabourke@gmail.com>
2018-05-18 21:10:41 +0000

    Done.

--- app.rb
+++ app.rb
@@ -7,18 +7,14 @@ class App < Sinatra::Base
   end
 
   get '/new' do
-    Puppy.new(params[input])
-    @name = Puppy[:name]
-    @breed = Puppy.new[:breed]
-    @age = Puppy.new
     erb :create_puppy
   end
 
-  post '/' do
-
+  post '/new' do
+    @name = params[:name]
+    @breed = params[:breed]
+    @age = params[:age]
+    erb :display_puppy
   end
-
-  end
-
 
 end
