1b8ee2b2688e5e6a67df3ff84bbd8da6a4701ddc
Brett Weisberg <weisberg.brett@gmail.com>
2018-06-21 10:48:14 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,20 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+
+  get '/NEW' do
+    erb :create_puppy
+
+  end
+
+  post '/' do
+    @name = params[:name]
+    @age = params[:age]
+    @breed = params[:breed]
+    erb :display_puppy
+  end
+
 end
