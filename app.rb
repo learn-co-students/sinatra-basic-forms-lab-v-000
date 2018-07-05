d48b930adedf8f5102a1aa4ab96e23254cb0b152
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-07-05 22:26:01 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :index.erb
+  end
+
+  get '/new' do
+    erb :create_puppy.erb
+  end
+
+  post '/' do
+    erb :disply_puppy.erb
+  end
 
 end
