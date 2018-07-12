bcd8197d3153a54efb980b5c83cd3963bc86f7f8
Brennan Fulmer <brennan.flmr@gmail.com>
2018-07-12 04:35:23 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,9 +1,20 @@
 require_relative 'config/environment'
+require_relative 'models/puppy.rb'
 
 class App < Sinatra::Base
 
   get '/' do
     erb :index
+  end
+  
+  get '/create_puppy' do
+    erb :create_puppy
+  end
+
+  post '/display_puppy' do
+    # puppy class interaction
+    
+    erb :display_puppy
   end
 
 end
