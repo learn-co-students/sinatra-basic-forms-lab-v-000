6c90f8738e2f135ed1d6005f5037efaf0b262aa3
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-07-24 22:35:36 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require_relative 'config/environment'
 
 class App < Sinatra::Base
@@ -10,7 +11,9 @@ class App < Sinatra::Base
     erb :create_puppy
   end
   
-  post '/' do
+  post '/display_puppy' do
+    @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+   
     erb :display_puppy
   end
     
