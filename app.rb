ba514e0481877baf2e544c7230c873b4f50662ef
treverandersen <trever.r.andersen@gmail.com>
2018-07-25 16:36:06 -0500

    added link to create_puppy page

--- app.rb
+++ app.rb
@@ -3,11 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		erb: index.erb
+		erb :index
 	end
 
 	get '/create_puppy' do 
-		erb: create_puppy.erb
+		erb :create_puppy
 	end
 
 	post '/display_puppy' do
