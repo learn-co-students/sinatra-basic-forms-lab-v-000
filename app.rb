7db030d7a613d22be9d8a8efbed2a81cad512813
Emily S <emily.sugano@gmail.com>
2018-07-31 18:29:39 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
 require_relative 'config/environment'
+require_relative 'models/puppy'
 
 class App < Sinatra::Base
 
+ get "/" do
+  erb :index
+ end
+
+ post "/" do
+   @params = params
+   erb :display_puppy
+ end
+
+ get "/new" do
+   erb :new
+ end
 end
