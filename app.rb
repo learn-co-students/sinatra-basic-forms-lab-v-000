6e8ffe92e1ad9722c9d8867d9937835c8e691879
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-09-11 13:37:20 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,22 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/new' do
+    name = params[:user_input][:name]
+    breed = params[:user_input][:breed]
+    age = params[:user_input][:age]
+    @puppy = Puppy.new(name, breed, age)
+    erb :display_puppy
+  end
 end
+
+
+# In app.rb build out a GET request to load a homepage. The homepage should go to the main route /
