50813da57ec2068058970b8d43a22ae3dec40f7a
Madeline Stark <madeline.stark1@gmail.com>
2018-09-12 15:34:25 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,19 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+
+  post '/' do
+    text_from_user = params
+    @analyzed_text = Puppy.new(text_from_user[:name], text_from_user[:breed], text_from_user[:age])
+    erb :display_puppy
+  end
+
 end
