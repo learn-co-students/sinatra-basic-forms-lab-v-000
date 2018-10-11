5271974897625bab4be1a4d2f093a008cfd3303d
Maurice Argoetti <maurice.argoetti@gmail.com>
2018-10-11 02:12:15 +0000

    Done.

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,11 +1,11 @@
 class Puppy
 
-  attr_accessor :name, :age, :breed
+  attr_accessor :name, :breed, :age
 
-  def initialize(name, age, breed)
+  def initialize(name, breed, age)
     @name = name
-    @age = age
     @breed = breed
+    @age = age
   end
   
 end
