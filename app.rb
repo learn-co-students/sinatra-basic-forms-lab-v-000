7931d6d2aa7ee9f1710124cf2acc75cf3db4595f
Melissa Real <melissa.real@hotmail.com>
2018-11-12 14:40:53 +0000

    Done.

--- app.rb
+++ app.rb
@@ -11,11 +11,7 @@ class App < Sinatra::Base
   end
 
   post '/puppy' do
-    muppy = Puppy.new(params[:name], params[:breed], params[:months_old])
-
-    @name = muppy.name
-    @breed = muppy.breed
-    @age = muppy.age
+    @muppy = Puppy.new(params[:name], params[:breed], params[:months_old])
 
     erb :display_puppy
   end
