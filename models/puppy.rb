912fb02d0b68e1e1ea616c302a91212486f3ee8c
Andrew Bernier <a6bernier@gmail.com>
2018-12-06 18:29:50 +0000

    Done.

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,6 +1,9 @@
-class Puppies
-
+class Puppy
   attr_accessor :name, :breed, :age
 
-
+  def initialize(name, breed, months_old)
+    @name = name
+    @breed = breed
+    @age = months_old
+  end
 end
