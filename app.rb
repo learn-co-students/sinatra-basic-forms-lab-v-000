cb78a8d452120a049b187c21510c6df50474b5d8
Rafael Lopez <lpz.raf@gmail.com>
2018-12-30 04:29:04 +0000

    Done.

--- app.rb
+++ app.rb
@@ -4,4 +4,11 @@ class App < Sinatra::Base
   get "/" do 
     erb :index
   end
+  
+  get "/new" do
+    @name = params[:name]
+    @breed = params[:breed]
+    @age = params[:age]
+    erb :index
+  end
 end
