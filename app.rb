76e2255e6d6e711de3912d60a64a9189ca6bc392
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-02-07 15:34:12 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,22 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+    get '/' do #gets a 200 status code
+
+        erb :index #satifies: has a link with the text "List A puppy" has a link to list a puppy that links to /new
+    end
+
+    get '/new' do
+
+        erb :create_puppy
+    end
+
+    post '/puppy' do
+        name = params[:name]
+        breed = params[:breed]
+        age = params[:age]
+        @pup = Puppy.new(name, breed, age)
+
+        erb :display_puppy
+    end
 end
