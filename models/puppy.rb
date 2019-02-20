5d920f30c993138b766f98682b39b22963a3ce6c
Heidi Hlavinka <hchlav@gmail.com>
2019-02-20 19:49:09 +0000

    Done.

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,9 +1,11 @@
 class Puppy
-  attr_accessor :name, :age
+  attr_accessor :age, :name
   attr_reader :breed
 
-  def initialize
-
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
   end
 
 end
