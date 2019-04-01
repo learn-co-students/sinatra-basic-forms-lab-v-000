aa99eb25932ed1fa79b297f5dce25dc72742fb33
Sheena Bonilla <sheena.myb@gmail.com>
2019-04-01 22:26:05 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,20 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
-
+  
+  get '/' do
+    erb :index
+  end
+  
+  get '/new' do
+    erb :create_puppy
+  end
+  
+  post '/puppy' do
+    hash = params
+    @puppy = Puppy.new(hash)
+    "#{@puppy}"
+    #erb :display_puppy  
+  end
 end
