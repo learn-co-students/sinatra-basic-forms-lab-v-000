d1c3a5bd9ec534f6fa75d81a06452224f7eb2093
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2019-07-04 22:34:47 +0000

    Done.

---
+++ models/puppy.rb
@@ -0,0 +1,22 @@
+
+class Puppy
+
+  def initialize(name, breed, months_old)
+    @puppy = name
+    @breed = breed
+    @month_old = months_old
+  end
+
+  def name
+    @puppy
+  end
+
+  def breed
+    @breed
+  end
+
+  def months_old
+    @months_old
+  end
+ name
+end
