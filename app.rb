77c6064f6e692911687ecd27ee74c591ff9457fa
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2019-07-08 04:15:15 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,15 +3,15 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-        erb :index
+    erb :index
       end
 
   get '/new' do
-        erb :create_puppy
+    erb :create_puppy
       end
 
   post '/puppy' do
-        erb :display_puppy
+    erb :display_puppy
       end
 
 end
