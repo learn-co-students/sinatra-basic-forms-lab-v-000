ba6a55176075d3971edee51734ff396859a92214
Elana Ryznar <eryzna@gmail.com>
2019-08-15 23:27:37 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,24 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+
+  post '/create_puppy' do
+    erb :create_puppy
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/puppy' do
+    @name = params[:name]
+    @breed = params[:breed]
+    @age = params[:age]
+
+    erb :display_puppy
+  end
+
 end
