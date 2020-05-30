9ea88d4702099a1ed316fc24db5526a8e36f582e
Neil Ryan <neilmarkoliverryan@gmail.com>
2020-05-30 11:12:42 -0700

    Completed lab

--- app.rb
+++ app.rb
@@ -2,4 +2,19 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do
+    "the /new route"
+    erb :create_puppy
+  end
+
+  post '/puppy' do
+    #raise params.inspect
+    @puppy = Puppy.new(name: params["name"] ,breed: params[:breed] ,age: params["age"])
+    erb :display_puppy
+  end
+
 end
